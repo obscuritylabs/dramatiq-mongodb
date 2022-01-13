5cb5106f20b7df52a112ec70cee3cb1a68df17ea
Tory Clasen <ToryClasen@Gmail.com>
2022-01-12 21:29:38 -0500

    fix: remove placeholder test that isn't doing anything
    Signed-off-by: Tory Clasen <ToryClasen@Gmail.com>

--- tests/test_package_minimum.py
+++
@@ -1,9 +0,0 @@
-from dramatiq_mongodb import __version__
-
-
-def test_version() -> None:
-    """Tests that the version is accessible and locked to a specific number.
-
-    This test will be removed when real tests are added.
-    """
-    assert __version__ == "0.1.0"  # Act
