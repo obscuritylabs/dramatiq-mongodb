11dc66427331c3121125b1379fdbe220aa2617b0
Tory Clasen <ToryClasen@gmail.com>
2022-03-31 12:14:26 -0400

    ci: remove mongo version test
    Signed-off-by: Tory Clasen <ToryClasen@gmail.com>

--- tests/test_mongodb.py
+++ tests/test_mongodb.py
@@ -1,11 +1,3 @@
-from pymongo.database import Database
-
-
-def test_mongodb_version(mongodb: Database) -> None:
-    """Test that the version of MongoDB used for testing is within tollerance."""
-    minimum_version = (3, 0, 0)
-    values = mongodb.client.server_info()["version"].split(".")
-
-    actual_version = tuple(map(int, values))  # Act
-
-    assert minimum_version <= actual_version
+def test_nothing() -> None:
+    """Tests nothing."""
+    assert True
